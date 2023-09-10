45df0b07d6adccf511fd2754a8db669ba23f07d1
lazygupta <guptashirley02@gmail.com>
2023-09-10 16:15:45 +0530

    .

--- Learn Javascript/script.js
+++ Learn Javascript/script.js
@@ -1,5 +1,25 @@
 console.log("Hello Ankit");
 
-name = "Ankit"
+names = "Ankit"
 
-console.log(name);
+// food= 20
+// tip= 0.2
+// tipAmount = food * tip
+// fruit = Number(prompt("Amount of food in rupees"))
+// tip = Number(prompt("Enter tip %")) / 100
+// tipAmount = fruit * tip
+// total = fruit + tipAmount
+// console.log("Tip amount " ,tipAmount)
+// console.log("Neeche total aaega")
+// console.log("Total ", total)
+
+weather = prompt ("What is the weather in your location")
+
+if(weather == "Rainy"){
+    console.log("Grab your Umbrella")
+}
+else {
+    console.log("Wear Sunglasses")
+}
+
+
