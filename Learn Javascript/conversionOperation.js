cf99c45008c70cf4f210aa20973637cb758cd259
Ankit <guptashirley02@gmail.com>
2024-02-26 23:06:35 +0530

    .

---
+++ Learn Javascript/conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "123abc"
+
+// console.log(typeof score)
+
+let valueinNumber = Number(score)
+// console.log(valueinNumber) // NaN => NotaNumber
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = 1
+
+let inBoolean = Boolean(isLoggedIn)
+console.log(typeof inBoolean)
+console.log(inBoolean)
+
+// "" => false
+// "ankit" => true
+
+let someNumber = 33
+let StringNumber = String(33)
+console.log(typeof StringNumber)
+console.log(StringNumber)
