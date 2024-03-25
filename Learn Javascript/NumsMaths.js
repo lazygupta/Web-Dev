56d2cbfd4b5f2a14cf0b309f2df4fe10330c88bc
Ankit <guptashirley02@gmail.com>
2024-03-26 01:32:41 +0530

    .

---
+++ Learn Javascript/NumsMaths.js
@@ -0,0 +1,14 @@
+const score = 400
+
+console.log(score)
+
+const balance = new Number(100)
+
+console.log(balance);
+
+console.log(Math.random());
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*10) + max)
