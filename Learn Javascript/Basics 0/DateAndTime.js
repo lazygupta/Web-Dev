b29b0f7e21cd1d81d534c2fe12029c99bc4b73e6
Ankit <guptashirley02@gmail.com>
2024-03-30 18:16:39 +0530

    Modified

---
+++ Learn Javascript/Basics 0/DateAndTime.js
@@ -0,0 +1,15 @@
+let mydate = new Date()
+
+// console.log(mydate.toString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toDateString())
+
+let createdate = new Date(2024,2,22,5,3) // YYYY,MM,DD
+
+// console.log(mydate.toLocaleString());
+
+let mytimestamp = Date.now()
+
+// console.log(createdate.getMonth());
+// console.log(Math.floor(Date.now()/1000));
+
